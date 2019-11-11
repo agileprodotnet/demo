180d7fa9d405379bcd9b41eef139ef9e0a14947f
david-guagile <david.r@quagilesolution.com>
2019-11-11 15:34:26 +0530

    added header and footer

--- tagy/src/App.js
+++ tagy/src/App.js
@@ -1,25 +1,22 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Header from './Layout/Header'
+import Footer from './Layout/Footer'
+import {
+  MDBEdgeHeader
+} from 'mdbreact'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit for <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+      <Header />
+      <div class="mt-3 mb-3">
+        Content
     </div>
+      <Footer />
+    </>
   );
 }
 
