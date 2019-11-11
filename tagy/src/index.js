020082b61d80ff05e6c1819aa221948ef270aff9
david-guagile <david.r@quagilesolution.com>
2019-11-11 17:25:32 +0530

    conflict resolved

--- tagy/src/index.js
+++ tagy/src/index.js
@@ -1,12 +1,3 @@
-<<<<<<< HEAD
-import React from "react";
-import ReactDOM from "react-dom";
-import "@fortawesome/fontawesome-free/css/all.min.css";
-import "bootstrap-css-only/css/bootstrap.min.css";
-import "mdbreact/dist/css/mdb.css";
-import "./index.css";
-import App from "./App";
-=======
 import React from 'react';
 import ReactDOM from 'react-dom';
 import "@fortawesome/fontawesome-free/css/all.min.css";
@@ -16,10 +7,10 @@ import "mdbreact/dist/css/mdb.css";
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
->>>>>>> 180d7fa9d405379bcd9b41eef139ef9e0a14947f
 
-import registerServiceWorker from './registerServiceWorker';
+ReactDOM.render(<App />, document.getElementById('root'));
 
-ReactDOM.render( <App /> , document.getElementById('root'));
-
-registerServiceWorker();
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
