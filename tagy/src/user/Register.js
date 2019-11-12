8633e20f1c2d8e090ac3c7fc11dfb26e84221d82
david-guagile <david.r@quagilesolution.com>
2019-11-12 14:38:34 +0530

    code align

--- tagy/src/user/Register.js
+++ tagy/src/user/Register.js
@@ -24,27 +24,27 @@ export default class Register extends React.Component {
                                         <p className="h5 text-center mb-4">Sign up</p>
                                         <label htmlFor="defaultFormRegisterNameEx" className="grey-text">
                                             Your name
-                </label>
+                                        </label>
                                         <input type="text" id="defaultFormRegisterNameEx" className="form-control" />
                                         <br />
                                         <label htmlFor="defaultFormRegisterEmailEx" className="grey-text">
                                             Your email
-                </label>
+                                        </label>
                                         <input type="email" id="defaultFormRegisterEmailEx" className="form-control" />
                                         <br />
                                         <label htmlFor="defaultFormRegisterConfirmEx" className="grey-text">
                                             Confirm your email
-                </label>
+                                        </label>
                                         <input type="email" id="defaultFormRegisterConfirmEx" className="form-control" />
                                         <br />
                                         <label htmlFor="defaultFormRegisterPasswordEx" className="grey-text">
                                             Your password
-                </label>
+                                        </label>
                                         <input type="password" id="defaultFormRegisterPasswordEx" className="form-control" />
                                         <div className="text-center mt-4">
                                             <button className="btn btn-unique" type="submit">
                                                 Register
-                  </button>
+                                            </button>
                                         </div>
                                     </form>
                                 </SectionContainer>
