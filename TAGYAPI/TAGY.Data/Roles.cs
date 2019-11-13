62ca7f973497d5dc8749e31acdb98a6ad868d485
veeramanibg <veeramani.b@quagilesolution.com>
2019-11-13 16:52:16 +0530

    TAGGYY.API Project Architecture

---
+++ TAGYAPI/TAGY.Data/Roles.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TAGY.Data
+{
+    public class Roles
+    {
+        public void SaveData()
+        {
+            var context = new TagyEntities();
+            var role = new Role()
+            {
+                RoleName = "nEW Admin user",
+                CreatedDateTime = DateTime.Now,
+                UpdatedDatetime = DateTime.Now
+            };
+            context.Roles.Add(role);
+
+            context.SaveChanges();
+        }
+    }
+}
